b0cf9e8bf89421ec9f2863a1504dcedbe761ea61
mihdan <mikhail@kobzarev.com>
2017-11-23 16:28:06 +0300

    Убрал BOM

--- mihdan-ticket-31245-patch.php
+++ mihdan-ticket-31245-patch.php
@@ -3,11 +3,13 @@
  * Plugin Name: Mihdan: Patch for ticket 31245
  * Description: WordPress-плагин, который убирает состояние гонки при обновлении кеша alloptions
  * Github Plugin URI: https://github.com/mihdan/mihdan-ticket-31245-patch/
- * Version: 1.0
+ * Version: 1.1
  */
- ?>
 
-<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 /**
  * Fix a race condition in alloptions caching
  *
@@ -17,8 +19,8 @@ function mihdan_ticket_31245_patch( $option ) {
     if ( ! wp_installing() ) {
         $alloptions = wp_load_alloptions(); //alloptions should be cached at this point
         if ( isset( $alloptions[ $option ] ) ) { //only if option is among alloptions
-		wp_cache_delete( 'alloptions', 'options' );
-	}
+            wp_cache_delete( 'alloptions', 'options' );
+        }
     }
 }
 add_action( 'added_option',   'mihdan_ticket_31245_patch' );
